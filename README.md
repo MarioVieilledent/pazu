9389824a33df6b2036e62cc7b4966e429b5b34d5
Mario <mario.vieilledent@gmail.com>
2025-09-26 11:34:24 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# Pazu
+
+Backend and Frontend for personal website hosted by Alwaysdata
+
+http://pazu.alwaysdata.net/
+
+## Installation
+
+- Install Golang
+- Run `go run .`
+- Build `go build`
---
+++ server.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+)
+
+func startServer() {
+	ip := os.Getenv("IP")
+	if ip == "" {
+		ip = "::"
+	}
+
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8100"
+	}
+
+	addr := fmt.Sprintf("[%s]:%s", ip, port)
+
+	fs := http.FileServer(http.Dir("./public"))
+
+	http.Handle("/", fs)
+
+	fmt.Printf("Starting server on http://%s\n", addr)
+
+	err := http.ListenAndServe(addr, nil)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+}
